5eb5ece056aa930ab198386ff42231b076f6a6ea
Jorge Lopez-Johnson <lopez.jr1995@gmail.com>
2018-03-02 16:56:40 -0500

    Authentication WIP

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+	namespace App\Http\Controllers;
+
+	class AuthController extends Controller {
+		/**
+		 * @return void
+		 */
+		public function __construct() {
+			$this->middleware('auth:api', ['except' => ['login']]);
+		}
+
+		/**
+		 * @return \Illuminate\Http\JsonResponse
+		 */
+		public function login() {
+			$credentials = request(['email', 'password']);
+
+			if (!$token = auth()->attempt($credentials))
+				return response()->json(['error' => 'Unauthorized'], 401);
+
+			return $this->respondWithToken($token);
+		}
+
+		/**
+		 * @return \Illuminate\Http\JsonResponse
+		 */
+		public function me() {
+			return response()->json(auth()->user());
+		}
+
+		/**
+		 * @return \Illuminate\Http\JsonResponse
+		 */
+		public function logout() {
+			auth()->logout();
+
+			return response()->json([
+				'message' => 'Successfully logged out',
+			]);
+		}
+
+		/**
+		 * @return \Illuminate\Http\JsonResponse
+		 */
+		public function refresh() {
+			return $this->respondWithToken(auth()->refresh());
+		}
+
+		/**
+		 * @param  string $token
+		 *
+		 * @return \Illuminate\Http\JsonResponse
+		 */
+		protected function respondWithToken($token) {
+			return response()->json([
+				'access_token' => $token,
+				'token_type' => 'bearer',
+				'expires_in' => auth()->factory()->getTTL() * 60,
+			]);
+		}
+	}
