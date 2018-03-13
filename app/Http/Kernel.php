1df203f3e983fb148cf51e98d0b945fee8ff7e34
jayar95 <lopez.jr1995@gmail.com>
2018-03-13 01:06:45 -0400

    Adding CORS support

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,6 +17,7 @@
 			\App\Http\Middleware\TrimStrings::class,
 			\Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
 			\App\Http\Middleware\TrustProxies::class,
+            \Barryvdh\Cors\HandleCors::class,
 		];
 
 		/**
