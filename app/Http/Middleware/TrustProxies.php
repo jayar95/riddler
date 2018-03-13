04310782fea68a8a7d754fc10dd90afed4c2b6c3
jayar95 <lopez.jr1995@gmail.com>
2018-03-13 00:32:00 -0400

    Recovering missing middleware

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -1,21 +1,23 @@
 <?php
-	namespace App\Http\Middleware;
 
-	use Illuminate\Http\Request;
-	use Fideloper\Proxy\TrustProxies as Middleware;
+namespace App\Http\Middleware;
 
-	class TrustProxies extends Middleware {
-		/**
-		 * The trusted proxies for this application.
-		 *
-		 * @var array
-		 */
-		protected $proxies;
+use Illuminate\Http\Request;
+use Fideloper\Proxy\TrustProxies as Middleware;
 
-		/**
-		 * The headers that should be used to detect proxies.
-		 *
-		 * @var string
-		 */
-		protected $headers = Request::HEADER_X_FORWARDED_ALL;
-	}
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * @var array
+     */
+    protected $proxies;
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var string
+     */
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+}
